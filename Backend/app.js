29cbaaff0c1537e864cb04625f7c615a5e612a67
Aditya Dixit <dixitaditya2005@gmail.com>
2025-04-16 17:47:53 +0530

    Fix CORS issues and update manifest

--- Backend/app.js
+++ Backend/app.js
@@ -17,13 +17,13 @@ app.use(bodyParser.json());
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://pixit-delta.vercel.app');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
