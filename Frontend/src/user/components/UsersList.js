becbaab112c8272c77cf1f9307da4f8a0e8a793f
Aditya Dixit <dixitaditya2005@gmail.com>
2025-04-16 17:09:49 +0530

    Add Railway configuration and update package.json

---
+++ Frontend/src/user/components/UsersList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
+import './UsersList.css';
+
+const UsersList = props => {
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <div className="users-grid">
+      {props.items.map((user, index) => (
+        <div 
+          key={user.id} 
+          className={`user-item ${index % 2 === 0 ? 'float-left-to-right' : 'float-right-to-left'}`}
+          style={{ animationDelay: `${index * 0.5}s` }}
+        >
+          <UserItem
+            id={user.id}
+            image={user.image}
+            name={user.name}
+            placeCount={user.places.length}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default UsersList;
